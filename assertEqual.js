64cdabde1b200341e50b31ce0b389a850903604c
Bayan Safaee <bayan.safaee@gmail.com>
2022-06-27 22:51:41 -0700

    uncommented test cases

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,8 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-/*
+
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 2);
-*/ 
