f9ce9b959a5e60c4e8add7625c85455508b1f400
Bayan Safaee <bayan.safaee@gmail.com>
2022-06-30 12:27:58 -0700

    function to count the number of each letter in a string

---
+++ countLetter.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const results = {};
+  const stringMod = (string.replace(/\s+/g, ''))
+  for (const letter in stringMod) { 
+    if (results[stringMod[letter]] === undefined) {
+      results[stringMod[letter]] = 1;
+    } else {
+      results[stringMod[letter]] += 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
