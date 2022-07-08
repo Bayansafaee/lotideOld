72550a966c2c03f7683a4d20203ddec741e5a8ac
Bayan Safaee <bayan.safaee@gmail.com>
2022-07-08 13:58:37 -0700

    created assertArrayEqualTest.js and modularized assertArrayEqual.js

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArrayEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
@@ -18,8 +8,4 @@ const assertArrayEqual = (arr1, arr2) => {
   }
 };
 
-
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
-assertArrayEqual([1, 2, 3], [3, 2, 1]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArrayEqual;
---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,6 @@
+const assertArrayEqual = require('../assertArrayEqual');
+
+assertArrayEqual([1, 2, 3], [1, 2, 3]);
+assertArrayEqual([1, 2, 3], [3, 2, 1]);
+assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
