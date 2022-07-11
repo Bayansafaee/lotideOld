edda9ef839fecbb9a7a0a0f3ab39595ec9cc4f9b
Bayan Safaee <bayan.safaee@gmail.com>
2022-07-11 09:33:39 -0700

    Refactored tailTest.js and midleTest.js file to use Mocha & Chai.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,18 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-const assertArrayEqual = require('../assertArrayEqual');
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for [1,2,]", () => {
+    assert.deepEqual(middle([1,2,]), []);
+  });
+  it("returns [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);;
+  });
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
 
-assertArrayEqual(middle([1]), []);
-assertArrayEqual(middle([1]), [1]);
-assertArrayEqual(middle([1,2]), []);
-assertArrayEqual(middle([1,2]), [1,2]);
-assertArrayEqual(middle([1,2,3]), [2]);
-assertArrayEqual(middle([1,2,3]), []);
-assertArrayEqual(middle([1,2,3,4]), [2,3]);
-assertArrayEqual(middle([1,2,3,4]), []);
+});
